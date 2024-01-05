b8a512bde3fc22022bae67c8dba6e80ba352aa44
Billy19191 <bill.ratchanon@gmail.com>
2024-01-05 21:26:56 +0700

    feat: draft design chord position

---
+++ src/app/songs/page.tsx
@@ -0,0 +1,29 @@
+function songs() {
+  return (
+    <div>
+      <h1>Perfect</h1>
+
+      <div className="m-10">
+        <div>
+          {/* <div>D E G G B A G A</div> */}
+          <p>
+            I found a love
+            <span>G</span>for me<span>Em</span>
+          </p>
+        </div>
+        <p>
+          Darling just dive<span>C</span>right in, And follow my lead
+          <span>D</span>
+        </p>
+        <p>
+          Well I found a girl<span>G</span>, beautiful<span>Em</span>and sweet
+        </p>
+        <p>
+          I never knew<span>C</span> you were the someone Waiting for me
+          <span>D</span>
+        </p>
+      </div>
+    </div>
+  )
+}
+export default songs
