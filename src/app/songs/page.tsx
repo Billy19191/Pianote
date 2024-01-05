b045402acaf517b5e38eaec3cc11451928cd7b39
Billy191 <bill.ratchanon@gmail.com>
2024-01-06 00:41:07 +0700

    feat: test loop lyric, chord, melody

--- src/app/songs/page.tsx
+++ src/app/songs/page.tsx
@@ -1,16 +1,33 @@
 function songs() {
+  const song = [
+    {
+      chord: ['G', 'Em'],
+      lyric: ['I found the love', 'for me'],
+      melody: ['D', 'E', 'G', 'G', 'B', 'A', 'G', 'B'],
+    },
+  ]
   return (
     <div>
-      <h1>Perfect</h1>
+      <h1 className="font-bold text-lg text-center mt-6">Perfect</h1>
 
       <div className="m-10">
-        <div>
-          {/* <div>D E G G B A G A</div> */}
-          <p>
-            I found a love
-            <span>G</span>for me<span>Em</span>
-          </p>
-        </div>
+        {/* <p>
+          I found a love
+          <span>G</span>for me<span>Em</span>
+        </p> */}
+        {song.map((each) => (
+          <div>
+            <div id="melody">{each.melody}</div>
+            {each.lyric.map((subeach, index) => (
+              <span>
+                <span key={index}>
+                  {subeach} <span id="chord">{each.chord[index]}</span>
+                </span>
+              </span>
+            ))}
+          </div>
+        ))}
+        {/* 
         <p>
           Darling just dive<span>C</span>right in, And follow my lead
           <span>D</span>
@@ -21,7 +38,7 @@ function songs() {
         <p>
           I never knew<span>C</span> you were the someone Waiting for me
           <span>D</span>
-        </p>
+        </p> */}
       </div>
     </div>
   )
